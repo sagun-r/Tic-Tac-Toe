a1f12f5eba359193998ce31fae8f6114d628d795
AmazingBandit <11012938+sagun-r@users.noreply.github.com>
2024-03-22 14:43:03 -0700

    Set up basic objects

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const Gameboard = () => {
+  const rows = 3;
+  const columns = 3;
   const gameboard = [];
 
   const players = {
     playerOne: "Player One",
     playerTwo: "Player Two",
   };
-  return { players, gameboard };
+
+  for (i = 0; i < rows; i++) {
+    gameboard.push([]);
+    for (j = 0; j < columns; j++) {
+      gameboard[i].push("test");
+    }
+  }
+  console.table(gameboard);
 };
+
+const GameController = () => {};
+
+Gameboard();
