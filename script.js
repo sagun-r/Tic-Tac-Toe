ffbd9aa76f06e6473dee2c4540b0bf6011e3ce24
AmazingBandit <11012938+sagun-r@users.noreply.github.com>
2024-06-25 20:29:55 -0700

    Added buttons to show board

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ Gameboard = () => {
   const columns = 3;
   const gameboard = [];
 
-  const getGameboard = () => gameboard;
-
   for (i = 0; i < rows; i++) {
     gameboard.push([]);
     for (j = 0; j < columns; j++) {
-      gameboard[i].push(Spaces());
+      gameboard[i].push(Space());
     }
   }
+
+  const getGameboard = () => gameboard;
+
+  const putSymbol = (column, player) => {};
+
   return { getGameboard };
 };
 
-Spaces = () => {
-  const doSomething = () => {};
-  const doSomethingElse = () => {};
-  return { doSomething, doSomethingElse };
+Space = () => {
+  let value = 0;
+  const getValue = () => value;
+  const getSymbol = () => activePlayer.symbol;
+  return { getValue, getSymbol };
 };
 
 const GameController = (playerOneName = "Player One", playerTwoName = "Player Two") => {
+  // const gameboard = Gameboard();
+
   const players = [
     {
       name: playerOneName,
@@ -53,19 +59,35 @@ const ScreenController = () => {
   const game = GameController();
 
   const updateScreen = () => {
-    divBoard.textContent = "Testing";
-    divBoard.textContent = gameboard;
+    divBoard.textContent = "";
 
-    const gameboard = Gameboard().getGameboard;
+    const gameboard = Gameboard().getGameboard();
+
+    gameboard.forEach((row) => {
+      row.forEach((space, index) => {
+        const divBtn = document.createElement("button");
+
+        // divBtn.textContent = space.getValue();
+        console.log(space);
+        console.log(index);
+        divBoard.append(divBtn);
+        divBtn.addEventListener("click", () => {
+          alert(space.getSymbol());
+        });
+      });
+    });
+
+    console.log(gameboard);
     activePlayer = game.getActivePlayer();
 
     whosTurn.textContent = `${activePlayer.name}'s turn.`;
 
-    activePlayer = game.getActivePlayer();
     console.log(activePlayer.name);
   };
   updateScreen();
-  game.switchPlayer();
+  // game.switchPlayer();
+  activePlayer = game.getActivePlayer();
+  whosTurn.textContent = `${activePlayer.name}'s turn.`;
 };
 
 ScreenController();
